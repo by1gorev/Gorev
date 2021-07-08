4e08f1623da1258b44ca9b8b51f07c9ab902c7a8
by1gorev <87039876+by1gorev@users.noreply.github.com>
2021-07-08 18:29:20 +0300

    Start project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {component} from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <a></a>
+
+
+            <Technologies/>
+        </div>
+
+    );
+}
+
+
+export default App;
